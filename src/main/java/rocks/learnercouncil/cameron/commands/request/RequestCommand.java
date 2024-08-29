ddee8d5a4f87ccc529774324227e027ca073fe46
H2ofiremaster <boomertnt628@gmail.com>
2024-08-28 20:49:26 -0700

    Removed a ton of unused imports

--- src/main/java/rocks/learnercouncil/cameron/commands/request/RequestCommand.java
+++ src/main/java/rocks/learnercouncil/cameron/commands/request/RequestCommand.java
@@ -1,16 +1,9 @@
 package rocks.learnercouncil.cameron.commands.request;
 
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.jetbrains.annotations.NotNull;
-import rocks.learnercouncil.cameron.Cameron;
 
-import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class RequestCommand extends ListenerAdapter {
